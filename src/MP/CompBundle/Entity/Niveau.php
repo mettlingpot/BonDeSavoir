ef214b2c0df6669232de78f18221e4ae564c7431
mettlingpot <mettlingpot@hotmail.com>
2017-09-04 23:12:22 +0200

    niveau

---
+++ src/MP/CompBundle/Entity/Niveau.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace MP\CompBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Niveau
+ *
+ * @ORM\Table(name="niveau")
+ * @ORM\Entity(repositoryClass="MP\CompBundle\Repository\NiveauRepository")
+ */
+class Niveau
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="color", type="string", length=255)
+     */
+    private $color;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Niveau
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set color
+     *
+     * @param string $color
+     *
+     * @return Niveau
+     */
+    public function setColor($color)
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    /**
+     * Get color
+     *
+     * @return string
+     */
+    public function getColor()
+    {
+        return $this->color;
+    }
+}
+
